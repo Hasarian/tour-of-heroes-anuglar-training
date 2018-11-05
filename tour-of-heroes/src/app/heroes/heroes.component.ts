319490758522bed58dccaa02a046cdf4a38b768b
hasarian <romain.demoustiez@gmail.com>
2018-11-05 12:27:38 +0100

    labo 5-11-2018

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import {HeroService} from '../hero.service';
+import {Hero} from '../hero'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  constructor() { }
+  heroes:Hero[];
+  selectedHero:Hero;
+  daoHero:HeroService;
+  constructor(private heroService:HeroService) {
+      this.daoHero=heroService;
+   }
 
   ngOnInit() {
+    this.getHeroes();
   }
-
+  onSelect(hero:Hero):void{
+    this.selectedHero=hero;
+  }
+  getHeroes():void{
+      this.daoHero.getHeroes().subscribe(heroes=>this.heroes=heroes);
+  }
 }
---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[]=
+[
+    {id:1,name:"Mr. Nice"},
+    {id:2,name:"All Might"},
+    {id:3,name:"Midorya"},
+    {id:4,name:"Nicolas"}
+];
