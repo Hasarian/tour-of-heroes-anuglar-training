5045db867440be1ba8f9126b30bd4f1b06fbd215
Hasarian <romain.demoustiez@gmail.com>
2018-11-07 00:26:49 +0100

    routing through app

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -8,15 +8,11 @@ import {Hero} from '../hero'
 })
 export class HeroesComponent implements OnInit {
   heroes:Hero[];
-  selectedHero:Hero;
   constructor(private heroService:HeroService) {
    }
 
   ngOnInit() {
     this.getHeroes();
-  }
-  onSelect(hero:Hero):void{
-    this.selectedHero=hero;
   }
   getHeroes():void{
       this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes);
--- tour-of-heroes/src/app/mock-heroes.ts
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -5,5 +5,7 @@ export const HEROES: Hero[]=
     {id:1,name:"Mr. Nice"},
     {id:2,name:"All Might"},
     {id:3,name:"Midorya"},
-    {id:4,name:"Nicolas"}
+    {id:4,name:"Nicolas"},
+    {id:5,name:"Thas Mock"},
+    {id:6,name:"giua"}
 ];
