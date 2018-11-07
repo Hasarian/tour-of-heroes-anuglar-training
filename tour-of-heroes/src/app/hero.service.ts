a2408692831c9dedf8f9dfcaa2630f9838069e39
Hasarian <romain.demoustiez@gmail.com>
2018-11-07 21:29:20 +0100

    add and delete heroes

--- tour-of-heroes/src/app/hero.service.ts
+++ tour-of-heroes/src/app/hero.service.ts
@@ -5,6 +5,10 @@ import {Observable,of} from 'rxjs';
 import {MessageService} from './message.service';
 import {catchError,map,tap} from 'rxjs/operators';
 
+const httpOptions={
+  headers:new HttpHeaders({'Constent-Type':'application/json'})
+};
+
 @Injectable({
   providedIn: 'root'
 })
@@ -41,5 +45,30 @@ export class HeroService {
 
       return of(result as T);
     };
+  }
+  updateHero(hero:Hero):Observable<any>{
+    return this.http.put(this.heroesUrl,hero,httpOptions)
+    .pipe(
+      tap(_=>this.log(`updated hero id= ${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+  addHero(hero:Hero):Observable<Hero>{
+      return this.http.post<Hero>(this.heroesUrl,hero,httpOptions)
+      .pipe(
+        tap((hero:Hero)=>this.log(`added hero w/ id= ${hero.id}`)),
+        catchError(this.handleError<Hero>('addhero'))
+      );
+  }
+  deleteHero(hero:Hero|number):Observable<Hero>
+  {
+    const id= typeof hero === 'number'? hero:hero.id;
+    const url=`${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url,httpOptions)
+    .pipe(
+      tap(_=>this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>(`deleteHero`))
+    );
   }
 }
